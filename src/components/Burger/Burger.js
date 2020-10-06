21f78642865d0a9a167ce4c0ffbc091d553e632e
amiamitswe <amiamitswe@gmail.com>
2020-10-06 22:28:15 +0600

    first commit

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import BurgerIngredient from './Burgeringredient/Burgeringredient'
+
+import classes from './Burger.module.css'
+
+const burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+    return [...Array(props.ingredients[igKey])].map((_, index) => {
+      return <BurgerIngredient key={igKey + index} type={igKey} />
+    })
+  })
+
+  console.log(props)
+  console.log(props.ingredients)
+  console.log(transformedIngredients)
+
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top"/>
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom"/>
+    </div>
+  )
+}
+
+export default burger
